a6647847d7cf708d883c24869a600ca95c6876cf
hmtsai <rarmicrozip@outlook.com>
2024-08-15 21:19:30 +0800

    Initral

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+<div>
+<h1>404</h1>
+<p>此地荒芜无一物。</p>
+<p><a href="<?php $this->options->siteUrl(); ?>">← 还于主页。</a></p>
+</div>
+<?php $this->need('footer.php'); ?>
